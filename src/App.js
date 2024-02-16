a4ad542248030cdc5b706e0b66f311e45b035cd5
implodingduck <zsek320@gmail.com>
2024-02-16 10:24:08 -0600

    updates to allow service tags and adjust json usability

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import CIDRUtils from './CIDRUtils';
 import servicetags from './ServiceTags_Public.json'
+import JsonView from '@uiw/react-json-view';
 
 function App() {
   const cidrUtils = new CIDRUtils()
@@ -12,22 +13,39 @@ function App() {
   const performIpLookup = (ip) => {
     let ipspace = ip
     const retval = []
+    
     if (ipspace.indexOf('/') === -1){
       ipspace = `${ipspace}/32`
     }
-    for (let v of servicetags.values){
-      //console.log(v)
-      for (let p of v.properties.addressPrefixes){
-        //console.log(p)
-        if (p.indexOf(':') === -1 && cidrUtils.isInRange(ipspace, p)){
-          retval.push(v)
+    try{
+      for (let v of servicetags.values){
+        //console.log(v)
+        for (let p of v.properties.addressPrefixes){
+          //console.log(p)
+          if (p.indexOf(':') === -1 && cidrUtils.isInRange(ipspace, p)){
+            retval.push(v)
+          }
         }
       }
+    }catch(error){
+      if(error.message === 'Invalid CIDR'){
+        console.log("lets try service tags")
+        ipspace = ipspace.replace("/32", "")
+        for (let v of servicetags.values){
+          if (v.name.toLowerCase().indexOf(ipspace.toLowerCase()) !== -1){
+            retval.push(v)
+          }
+        }
+        
+      }
     }
-    if (retval.length === 0){
-      return `IP Space ${ipspace} not in Azure Service Tags list`
-    }
-    return `IP Space ${ipspace} is in: \n${JSON.stringify(retval, null, 2)}`
+
+    
+    // if (retval.length === 0){
+    //   return `IP Space ${ipspace} not in Azure Service Tags list`
+    // }
+    // return `IP Space ${ipspace} is in: \n${JSON.stringify(retval, null, 2)}`
+    return JSON.stringify(retval, null, 2)
     
   }
 
@@ -52,16 +70,14 @@ function App() {
     <div className="App">
       <fieldset>
       <legend>Is IP Azure?</legend>
-      <label htmlFor='ipspace'>IP or CIDR:</label>
+      <label htmlFor='ipspace'>IP or CIDR or ServiceTag:</label>
       <input type="text" onChange={handleIpSpaceChange} name="ipspace" value={ipspace}/>
       <button onClick={checkIp}>Check!</button>
       </fieldset>
       { (results.length === 0) ? "" :
       (<fieldset>
         <legend>Results</legend>
-        <pre>
-          {results}
-        </pre>
+        <JsonView value={JSON.parse(results)} />
       </fieldset>)
     }
     </div>
